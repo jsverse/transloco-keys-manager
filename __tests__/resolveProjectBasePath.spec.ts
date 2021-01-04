d3984c58b0d68e6b4925237c9634be2fac34036e
Josh Tate <joshtate04@icloud.com>
2021-01-04 14:31:00 -0500

    test: added tests for workspace config path

--- __tests__/resolveProjectBasePath.spec.ts
+++ __tests__/resolveProjectBasePath.spec.ts
@@ -3,8 +3,8 @@ import * as fs from 'fs-extra';
 import { resolveProjectBasePath } from '../src/helpers/resolveProjectBasePath';
 
 describe('resolveProjectBasePath', () => {
-  function addAngularConfig() {
-    fs.writeJsonSync('angular.json', {
+  function addAngularConfig(configFile: string = 'angular.json') {
+    fs.writeJsonSync(configFile, {
       defaultProject: 'defaultProject',
       projects: {
         defaultProject: { projectType: 'application', sourceRoot: 'testDir' },
@@ -13,32 +13,37 @@ describe('resolveProjectBasePath', () => {
     });
   }
 
-  function removeAngularConfig() {
-    fs.removeSync('angular.json');
+  function removeAngularConfig(configFile: string = 'angular.json') {
+    fs.removeSync(configFile);
   }
 
   it('should return the default "src"', () => {
     expect(resolveProjectBasePath().projectBasePath).toBe('src');
   });
 
-  describe('with angular config', () => {
-    beforeAll(() => {
-      addAngularConfig();
-    });
-    afterAll(() => {
-      removeAngularConfig();
-    });
+  describe.each([
+    [undefined, 'angular.json'],
+    ['workspace.json', 'workspace.json']
+  ])('with workspace config as %s', (workspaceConfigPath, configFileName) => {
+    describe('with angular config', () => {
+      beforeAll(() => {
+        addAngularConfig(configFileName);
+      });
+      afterAll(() => {
+        removeAngularConfig(configFileName);
+      });
 
-    it('should return the source root of the default project', () => {
-      const { projectBasePath, projectType } = resolveProjectBasePath();
-      expect(projectBasePath).toBe('testDir');
-      expect(projectType).toBe('application');
-    });
+      it('should return the source root of the default project', () => {
+        const { projectBasePath, projectType } = resolveProjectBasePath(undefined, workspaceConfigPath);
+        expect(projectBasePath).toBe('testDir');
+        expect(projectType).toBe('application');
+      });
 
-    it('should return the source root of the given project', () => {
-      const { projectBasePath, projectType } = resolveProjectBasePath('myProject');
-      expect(projectBasePath).toBe('myRoot');
-      expect(projectType).toBe('library');
+      it('should return the source root of the given project', () => {
+        const { projectBasePath, projectType } = resolveProjectBasePath('myProject', workspaceConfigPath);
+        expect(projectBasePath).toBe('myRoot');
+        expect(projectType).toBe('library');
+      });
     });
   });
 });
