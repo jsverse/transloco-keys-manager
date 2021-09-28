4f83fc361764cb4eb2b291c93ae8d8fb5fe27860
Evan Payne <evan@evanpayne.com>
2021-09-28 11:23:49 +0200

    fixes https://github.com/ngneat/transloco-keys-manager/issues/109

--- src/keys-builder/template/structural-directive.extractor.ts
+++ src/keys-builder/template/structural-directive.extractor.ts
@@ -14,6 +14,7 @@ import { resolveAliasAndKey } from '../utils/resolvers.utils';
 
 import { TemplateExtractorConfig } from './types';
 import {
+  isBindingPipe,
   isBoundText,
   isConditionalExpression,
   isElement,
@@ -75,6 +76,7 @@ export function traverse(
 
 function getMethodUsages(expressions: AST[], containers: ContainerMetaData[]) {
   return expressions
+    .map((exp) => (isBindingPipe(exp) ? exp.exp : exp))
     .filter((exp) => isTranslocoMethod(exp, containers))
     .map((exp: MethodCall) => {
       return {
