953deaa90a4a96b009f024124b182271ce6af86a
shaharkazaz <shahar.kazaz@gmail.com>
2021-10-31 09:30:24 +0200

    feat: 🎸 support workspace.json configs
    ✅ Closes: #72

--- __tests__/resolveProjectBasePath.spec.ts
+++ __tests__/resolveProjectBasePath.spec.ts
@@ -2,44 +2,70 @@ import * as fs from 'fs-extra';
 
 import { resolveProjectBasePath } from '../src/utils/resolve-project-base-path';
 
+function jsonFile(name: string) {
+  return name + '.json';
+}
+
+const defaultConfig = {
+  defaultProject: 'defaultProject',
+  projects: {
+    defaultProject: { projectType: 'application', sourceRoot: 'testDir' },
+    myProject: { projectType: 'library', sourceRoot: 'myRoot' },
+  },
+};
+
 describe('resolveProjectBasePath', () => {
-  function addAngularConfig() {
-    fs.writeJsonSync('angular.json', {
-      defaultProject: 'defaultProject',
-      projects: {
-        defaultProject: { projectType: 'application', sourceRoot: 'testDir' },
-        myProject: { projectType: 'library', sourceRoot: 'myRoot' },
-      },
-    });
+  const supportedConfigs = ['angular', 'workspace'];
+
+  function addConfig(configType: string, config: any = defaultConfig) {
+    fs.writeJsonSync(jsonFile(configType), config);
   }
 
-  function removeAngularConfig() {
-    fs.removeSync('angular.json');
+  function removeAngularConfig(configType: string) {
+    fs.removeSync(jsonFile(configType));
+  }
+
+  function assertDefaultProject() {
+    const { projectBasePath, projectType } = resolveProjectBasePath();
+    expect(projectBasePath).toBe('testDir');
+    expect(projectType).toBe('application');
   }
 
   it('should return the default "src"', () => {
     expect(resolveProjectBasePath().projectBasePath).toBe('src');
   });
 
-  describe('with angular config', () => {
-    beforeAll(() => {
-      addAngularConfig();
-    });
-    afterAll(() => {
-      removeAngularConfig();
-    });
+  it('should work when having both Angular and Workspace config', () => {
+    addConfig('angular');
+    addConfig('workspace', { otherStuff: true });
+    assertDefaultProject();
+    supportedConfigs.forEach(removeAngularConfig);
+    addConfig('workspace');
+    addConfig('angular', { otherStuff: true });
+    assertDefaultProject();
+    supportedConfigs.forEach(removeAngularConfig);
+  });
 
-    it('should return the source root of the default project', () => {
-      const { projectBasePath, projectType } = resolveProjectBasePath();
-      expect(projectBasePath).toBe('testDir');
-      expect(projectType).toBe('application');
-    });
+  supportedConfigs.forEach((configType) => {
+    describe(`${configType} config`, () => {
+      beforeAll(() => {
+        addConfig(configType);
+      });
 
-    it('should return the source root of the given project', () => {
-      const { projectBasePath, projectType } =
-        resolveProjectBasePath('myProject');
-      expect(projectBasePath).toBe('myRoot');
-      expect(projectType).toBe('library');
+      afterAll(() => {
+        supportedConfigs.forEach(removeAngularConfig);
+      });
+
+      it('should return the source root of the default project', () => {
+        assertDefaultProject();
+      });
+
+      it('should return the source root of the given project', () => {
+        const { projectBasePath, projectType } =
+          resolveProjectBasePath('myProject');
+        expect(projectBasePath).toBe('myRoot');
+        expect(projectType).toBe('library');
+      });
     });
   });
 });
--- src/utils/resolve-project-base-path.ts
+++ src/utils/resolve-project-base-path.ts
@@ -1,24 +1,45 @@
+import chalk from 'chalk';
 import { cosmiconfigSync } from 'cosmiconfig';
 
 import { ProjectType } from '../config';
+
+const angularConfig = ['angular.json', '.angular.json'];
+const workspaceConfig = ['workspace.json'];
+
+function searchConfig(searchPlaces: string[]) {
+  return cosmiconfigSync('', { searchPlaces }).search()?.config;
+}
 
 export function resolveProjectBasePath(projectName?: string): {
   projectBasePath: string;
   projectType: ProjectType;
 } {
-  const result = cosmiconfigSync('angular', {
-    searchPlaces: ['angular.json', '.angular.json'],
-  }).search();
+  const angular = searchConfig(angularConfig);
+  const workspace = searchConfig(workspaceConfig);
   let sourceRoot = 'src';
   let projectType;
-  const config = result?.config;
-  if (config) {
-    projectName = projectName || config.defaultProject;
-    const project = config.projects[projectName];
-    if (project) {
-      sourceRoot = project.sourceRoot;
-      projectType = project.projectType;
+
+  for (const config of [angular, workspace]) {
+    if (config) {
+      projectName = projectName || config.defaultProject;
+      const project = config.projects?.[projectName];
+      if (project) {
+        sourceRoot = project.sourceRoot;
+        projectType = project.projectType;
+        break;
+      }
     }
+  }
+
+  if (!angular && !workspace) {
+    const searchPlaces = angularConfig.concat(workspaceConfig);
+    console.log(
+      chalk.black.bgRed(
+        `Unable to load workspace config from ${searchPlaces.join(
+          ', '
+        )}. Defaulting source root to '${sourceRoot}'`
+      )
+    );
   }
 
   return { projectBasePath: sourceRoot, projectType };
