b229e409133712d3cf0d5381e21538cbd86b1473
AbdealiJK <abdealikothari@gmail.com>
2022-04-03 06:07:53 +0530

    feat: 🎸 Allow method calls with pipes
    When we used method calls with pipes, the transloco key was being
    ignored by the extract utility. This was because we were not parsing the
    MethodCall nodes from the AST. Add method call with pipes as transloco
    supports that.
    
    ✅ Closes: #129

--- src/keys-builder/template/pipe.extractor.ts
+++ src/keys-builder/template/pipe.extractor.ts
@@ -14,6 +14,7 @@ import {
   isInterpolation,
   isLiteralExpression,
   isLiteralMap,
+  isMethodCall,
   isTemplate,
   parseTemplate,
 } from './utils';
@@ -76,7 +77,9 @@ function getPipeValuesFromAst(ast: AST): AST[] {
     exp = [ast.trueExp, ast.falseExp];
   } else if (isBinaryExpression(ast)) {
     exp = [ast.left, ast.right];
-  } 
+  } else if (isMethodCall(ast)) {
+    exp = [...ast.args, ast.receiver];
+  }
 
   return exp.map(getPipeValuesFromAst).flat();
 }
